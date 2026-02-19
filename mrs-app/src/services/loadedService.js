8bdf5fc3be07ca0bcf931baf31544a846a5c47e2
12Khunny <kunnawut.spp@gmail.com>
2026-02-19 11:29:10 +0700

    SetZero

--- mrs-app/src/services/loadedService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function getLoadedTruckDetail({ apiUrl, token }) {
-  const res = await axios.get(`${apiUrl}/loadedTruck/loadedTruckDetail`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return res.data; // { truck_list, coop_list, default_payment_calculate_type }
-}
